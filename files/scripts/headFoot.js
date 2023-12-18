6bc525db76cbf3f827d02c156594fdabb77132fd
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2023-12-18 11:42:41 -0800

    Create headFoot.js

---
+++ files/scripts/headFoot.js
@@ -0,0 +1,9 @@
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
+function runHead(imlink){
+	header.innerHTML = "<img src='"+imLink+"'>";
+	footer.innerHTML = "<p class='disclaimer'>Powered by Aztech Admit <em>Secure Ticketing Services</em></p> <p><a href=''>Terms of Service</a> | <a href=''>Cookie Settings</a> | <a href=''>Security</a> | <a href=''>Report Issue</a>";
+	
+	
+}
